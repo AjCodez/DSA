9f34ec10fd4ff90ea638459d96555ebfe8db0ee5
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-01-30 15:43:02 +0530

    Added new file

---
+++ LinkedList/Sll_delete.java
@@ -0,0 +1,75 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class Main {
+    Node head;
+
+    public void insertAtBeg(int x) {
+        Node n = new Node(x);
+        n.next = head;
+        head = n;
+    }
+
+    public void insertAtEnd(int x) {
+        Node n = new Node(x);
+        if (head == null) {
+            head = n;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = n;
+        }
+    }
+
+    public int count() {
+        int c = 0;
+        Node temp = head;
+        while (temp != null) {
+            c++;
+            temp = temp.next;
+        }
+        return c;
+    }
+
+    public void deleteAtBeg() {
+        if (head == null)
+            return;
+        else {
+            head = head.next;
+        }
+    }
+
+    public void deleteAtEnd() {
+        if (head == null)
+            return;
+        else {
+            Node temp = head;
+            if (temp.next == null) {
+                head = null;
+            } else {
+                while (temp.next.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        Node n1 = new Node(10);
+        obj.head = n1;
+        Node n2 = new Node(20);
+        n1.next = n2;
+        Node n3 = new Node(30);
+        n2.next = n3;
+    }
+}
