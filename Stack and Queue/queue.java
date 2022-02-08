3eec04f539b2cb1aeacea5408dcbe2ed40d19a0e
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-02-08 15:15:44 +0530

    updated queue

--- Stack and Queue/queue.java
+++ Stack and Queue/queue.java
@@ -1,61 +1,61 @@
 class Node{
-    int data;
-    Node next;
-    Node(int data){
-      this.data=data;
+  int data;
+  Node next;
+  Node(int data){
+    this.data=data;
+  }
+}
+public class queue {
+  Node front;
+  Node rear;
+  boolean isEmpty(){
+    if(front==null)
+      return true;
+    else
+      return false;
+  }
+  public void enqueue(int data){
+    Node n = new Node(data);
+    if(isEmpty()){
+      front=n;
+      rear=n;
+    }
+    else{
+      rear.next=n;
+      rear=n;
     }
   }
-  public class queue {
-    Node front;
-    Node rear;
-    boolean isEmpty(){
-      if(front==null)
-        return true;
-      else
-        return false;
+  public int dequeue(){
+    if(isEmpty()){
+      System.out.println("queue is empty");
+      return -1;
     }
-    public void enqueue(int data){
-      Node n = new Node(data);
-      if(isEmpty()){
-        front=n;
-        rear=n;
-      }
-      else{
-        front.next=n;
-        front=n;
-      }
+    else{
+      int x=front.data;
+      front=front.next;
+      return x;
     }
-    public int dequeue(){
-      if(isEmpty()){
-        System.out.println("queue is empty");
-        return -1;
-      }
-      else{
-        int x=rear.data;
-        rear=rear.next;
-        return x;
-      }
+  }
+  public int front(){
+    if(isEmpty()){
+      System.out.println("Queue is empty");
+      return -1;
     }
-    public int front(){
-      if(isEmpty()){
-        System.out.println("Queue is empty");
-        return -1;
-      }
-      else{
-        int x=front.data;
-        return x;
-      }
+    else{
+      int x=front.data;
+      return x;
     }
-    public int rear(){
-      if(isEmpty()){
-        System.out.println("Queue is empty");
-        return -1;
-      }
-      else{
-        int x=rear.data;
-        return x;
-      }
+  }
+  public int rear(){
+    if(isEmpty()){
+      System.out.println("Queue is empty");
+      return -1;
     }
+    else{
+      int x=rear.data;
+      return x;
+    }
+  }
     public static void main(String[] args) {
         queue q = new queue();
         System.out.println(q.dequeue());
