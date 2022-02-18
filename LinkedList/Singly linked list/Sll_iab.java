3330d60ae77bbae7ddb88a7da524779971c832c6
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-02-18 19:50:29 +0530

    updated

--- LinkedList/Singly linked list/Sll_iab.java
+++ LinkedList/Singly linked list/Sll_iab.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
-class Node{
+
+class Node {
   int data;
   Node next;
-  Node(int data){
+
+  Node(int data) {
     this.data = data;
   }
 }
+
 public class Sll_iab {
   Node head;
-  public void insertAtBeg(int x){
+
+  public void insertAtBeg(int x) {
     Node n = new Node(x);
     n.next = head;
-      head = n;
+    head = n;
   }
-    public void print(){
-        Node temp = head;
-        while(temp!=null){
-                System.out.print(temp.data+" ");
-            temp=temp.next;
-    }}
-    public static void main(String[] args) {
-        Sll_iab obj = new Sll_iab();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            int temp = sc.nextInt();
-            obj.insertAtBeg(temp);
-        }
-        sc.close();
-        obj.print();
-        }
-      
+
+  public void print() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.data + " ");
+      temp = temp.next;
     }
+  }
+
+  public static void main(String[] args) {
+    Sll_iab obj = new Sll_iab();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      int temp = sc.nextInt();
+      obj.insertAtBeg(temp);
+    }
+    sc.close();
+    obj.print();
+  }
+
+}
