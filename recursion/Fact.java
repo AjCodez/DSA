6ddfc6404457197bd9180792235c29b6fe513c98
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-02-22 00:11:11 +0530

    created

---
+++ recursion/Fact.java
@@ -0,0 +1,13 @@
+class Fact{
+    static int fact(int n)
+    {
+        if (n <= 1) 
+            return 1;
+        else    
+            return n*fact(n-1);    
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fact(n));
+    }
+}
