919f173b742a7a133b3f5934aaf928452e32b1f8
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-02-27 01:54:56 +0530

    created

---
+++ recursion/PrintLLUsingRecursion.java
@@ -0,0 +1,14 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+public class PrintLLUsingRecursion{
+    Node head;
+
+    public static void insertAtEnd(int x){
+        
+    }
+}
