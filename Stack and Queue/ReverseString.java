3337b393ec70e5e10594fdc5dc3cc67f28bbf346
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-02-28 00:47:25 +0530

    made stack manually

--- Stack and Queue/ReverseString.java
+++ Stack and Queue/ReverseString.java
@@ -1,9 +1,55 @@
 import java.util.*;
-
+class Node1 {
+    char data;
+    Node1 next;
+  
+    Node1(char data) {
+      this.data = data;
+    }
+  }
+  
+  class Stack3 {
+    Node1 top;
+  
+    boolean isEmpty() {
+      if (top == null)
+        return true;
+      else
+        return false;
+    }
+  
+    public void push(char data) {
+      Node1 n = new Node1(data);
+      n.next = top;
+      top = n;
+    }
+  
+    public char pop() {
+      if (isEmpty()) {
+        System.out.println("Stack underflow");
+        return ' ';
+      } else {
+        char x = top.data;
+        top = top.next;
+        return x;
+      }
+    }
+  
+    public char peek() {
+      if (isEmpty()) {
+        System.out.println("Stack underflow");
+        return ' ';
+      } else {
+        char x = top.data;
+        return x;
+      }
+    }
+  }
+  
 public class ReverseString {
 
     public static void main(String[] args) {
-       Stack<Character> st = new Stack<Character>();
+       Stack3 st = new Stack3();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         for(int i=0;i<s.length();i++){
