ab9d0123d11cc56ca5ddb44e512bb2d8b15c20bd
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-24 22:58:58 +0530

    added quick sort

--- Searching and Sorting/QuickSort.java
+++ Searching and Sorting/QuickSort.java
@@ -1,49 +1,44 @@
-public class QuickSort {
+import java.util.Scanner;
 
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+public class QuickSort1 {
+
+    private static void swap(int [] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
     }
 
-    static int partition(int[] arr, int low, int high) {
-
-        int pivot = arr[high];
-
-        int i = (low - 1);
-
-        for (int j = low; j <= high - 1; j++) {
-
-            if (arr[j] < pivot) {
+    public static int partition(int [] arr, int l, int r) {
+        int i=l;
+        for(int f=l;f<r;f++){
+            if(arr[f]<arr[r]){
+                swap(arr,i,f);
                 i++;
-                swap(arr, i, j);
             }
         }
-        swap(arr, i + 1, high);
-        return (i + 1);
+        swap(arr,i,r);
+        return i;
     }
 
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+    public static void quickSort(int [] arr,int l, int r) {
+        if(l<r){
+            int p=partition(arr, l, r);
+            quickSort(arr, l, p-1);
+            quickSort(arr, p+1, r);
         }
-    }
-
-    static void printArray(int[] arr, int size) {
-        for (int i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr = { 10, 7, 8, 9, 1, 5 };
-        int n = arr.length;
-
-        quickSort(arr, 0, n - 1);
-        System.out.println("Sorted array: ");
-        printArray(arr, n);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        for (int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+        quickSort(arr, 0, n-1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
