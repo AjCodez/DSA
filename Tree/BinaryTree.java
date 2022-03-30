85f364520203930da0512acc91d069d1fa43218a
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-30 22:29:38 +0530

    fixed

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -4,19 +4,32 @@ class TreeNode {
 
     TreeNode(int data) {
         this.data = data;
+        this.left = null;
+        this.right = null;
     }
 
 }
 
 public class BinaryTree {
-
+    static TreeNode root;
+    BinaryTree(){
+        root = null;
+    }
     public static void Traverse(TreeNode Node) {
-        
-        
+        if(Node == null)
+            return; 
+        Traverse(Node.left);
+        Traverse(Node.right);
+        System.out.print(Node.data + " ");
     }
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10);
+        root = new TreeNode(10);
         root.left = new TreeNode(30);
-        root.right = new TreeNode(40);  
+        root.right = new TreeNode(40);
+        root.left.left = new TreeNode(50);
+        root.left.right = new TreeNode(60);
+        root.right.left = new TreeNode(55);
+        root.right.right = new TreeNode(65);
+        Traverse(root);
     }
 }
