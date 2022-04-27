69ac5c572b4e806e601f02cad8fcbbca8331f8a2
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-27 23:45:59 +0530

    added DFS

---
+++ Graph/DFS.java
@@ -0,0 +1,48 @@
+// DFS algorithm in Java
+
+import java.util.*;
+
+class DFS {
+  private LinkedList<Integer> adjLists[];
+  private boolean visited[];
+
+  // Graph creation
+  DFS(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
+  }
+
+  // Add edges
+  void addEdge(int src, int dest) {
+    adjLists[src].add(dest);
+  }
+
+  // DFS algorithm
+  void dfs(int vertex) {
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+
+    Iterator<Integer> ite = adjLists[vertex].listIterator();
+    while (ite.hasNext()) {
+      int adj = ite.next();
+      if (!visited[adj])
+      dfs(adj);
+    }
+  }
+
+  public static void main(String args[]) {
+    DFS g = new DFS(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+
+    System.out.println("Following is Depth First Traversal");
+
+    g.dfs(2);
+  }
+}
