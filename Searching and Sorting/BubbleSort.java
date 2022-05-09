d2395599812fc8fefbc90398318ab9dfd65c88e2
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-05-09 20:27:16 +0530

    ...

--- Searching and Sorting/BubbleSort.java
+++ Searching and Sorting/BubbleSort.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 public class BubbleSort {
     public static void bubbleSort(int array[]) {
-    int size = array.length;
-    for (int i = 0; i < size - 1; i++)
-        for (int j = 0; j < size - i - 1; j++)
+    int s = array.length;
+    for (int i = 0; i < s - 1; i++)
+        for (int j = 0; j < s - i - 1; j++)
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
