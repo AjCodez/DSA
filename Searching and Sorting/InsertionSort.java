ba8daa1ea1a69811161041e5626b2cad962ea17d
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-05-09 20:29:01 +0530

    ...

--- Searching and Sorting/InsertionSort.java
+++ Searching and Sorting/InsertionSort.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class InsertionSort {
 	public static void insertionSort(int arr[]) {
-		int n = arr.length;
-		for (int i = 1; i < n; ++i) {
+		int s = arr.length;
+		for (int i = 1; i < s; ++i) {
 			int key = arr[i];
 			int j = i - 1;
 			while (j >= 0 && arr[j] > key) {
