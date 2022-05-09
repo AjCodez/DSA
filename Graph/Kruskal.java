f6588294fd6176151de4272f7e07f80f2edf4175
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-05-10 04:25:17 +0530

    formatted

--- Graph/Kruskal.java
+++ Graph/Kruskal.java
@@ -1,8 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Edge implements  Comparable<Edge>
-{
+class Edge implements Comparable<Edge> {
   int v1;
   int v2;
   int weight;
@@ -15,69 +14,63 @@ class Edge implements  Comparable<Edge>
 
   @Override
   public int compareTo(Edge o) {
-    return this.weight-o.weight;
+    return this.weight - o.weight;
 
   }
 }
+
 public class Kruskal {
-  public static Edge[] kruskal(Edge edges[], int v)
-  {
+  public static Edge[] kruskal(Edge edges[], int v) {
     Arrays.sort(edges);
-    Edge mst[] = new Edge[v-1];
-    int count=0;
-    int i=0;
+    Edge mst[] = new Edge[v - 1];
+    int count = 0;
+    int i = 0;
     int parent[] = new int[v];
-    for (int j = 0; j <v ; j++) {
+    for (int j = 0; j < v; j++) {
       parent[j] = j;
     }
-    while(count != v-1)
-    {
+    while (count != v) {
 
       Edge currentedge = edges[i++];
-      int v1parent = findparent(currentedge.v1,parent);
+      int v1parent = findparent(currentedge.v1, parent);
       int v2parent = findparent(currentedge.v2, parent);
-      if(v1parent != v2parent)
-      {
+      if (v1parent != v2parent) {
         // including current edge
-        mst[count]= currentedge;
+        mst[count] = currentedge;
         count++;
-        parent[v1parent]= v2parent;
+        parent[v1parent] = v2parent;
       }
     }
-      return mst;
+    return mst;
   }
 
   private static int findparent(int v1, int[] parent) {
-    if(v1 == parent[v1])
-    {
+    if (v1 == parent[v1]) {
       return v1;
     }
-    return findparent(parent[v1],parent);
+    return findparent(parent[v1], parent);
   }
-  
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    // how many vertex 
+    // how many vertex
     int v = sc.nextInt();
     // how many edges
     int e = sc.nextInt();
     Edge edgearray[] = new Edge[e];
-    for (int i = 0; i <e ; i++) {
+    for (int i = 0; i < e; i++) {
       int v1 = sc.nextInt();
       int v2 = sc.nextInt();
       int weight = sc.nextInt();
-      Edge obj = new Edge(v1,v2,weight);
-      edgearray[i]= obj;
+      Edge obj = new Edge(v1, v2, weight);
+      edgearray[i] = obj;
     }
-    Edge[] mst = kruskal(edgearray,v);
-    for (int i = 0; i <mst.length ; i++) {
-      if(mst[i].v1<mst[i].v2)
-      {
-        System.out.println(mst[i].v1+" " +mst[i].v2+"  "+mst[i].weight);
-      }
-      else
-      {
-        System.out.println(mst[i].v2+" "+mst[i].v1+"  "+mst[i].weight);
+    Edge[] mst = kruskal(edgearray, v);
+    for (int i = 0; i < mst.length; i++) {
+      if (mst[i].v1 < mst[i].v2) {
+        System.out.println(mst[i].v1 + " " + mst[i].v2 + "  " + mst[i].weight);
+      } else {
+        System.out.println(mst[i].v2 + " " + mst[i].v1 + "  " + mst[i].weight);
       }
     }
     sc.close();
