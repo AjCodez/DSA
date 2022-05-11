8cdd99876ce062ac12f882fc1c3eaff002ea0253
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-05-11 21:27:31 +0530

    added new code

--- Graph/Kruskal.java
+++ Graph/Kruskal.java
@@ -1,78 +1,122 @@
-import java.util.Arrays;
-import java.util.Scanner;
+// Kruskal's algorithm in Java
 
-class Edge implements Comparable<Edge> {
-  int v1;
-  int v2;
-  int weight;
+import java.util.*;
 
-  public Edge(int v1, int v2, int weight) {
-    this.v1 = v1;
-    this.v2 = v2;
-    this.weight = weight;
-  }
+class Graph {
+  class Edge implements Comparable<Edge> {
+    int src, dest, weight;
 
-  @Override
-  public int compareTo(Edge o) {
-    return this.weight - o.weight;
-
-  }
-}
-
-public class Kruskal {
-  public static Edge[] kruskal(Edge edges[], int v) {
-    Arrays.sort(edges);
-    Edge mst[] = new Edge[v - 1];
-    int count = 0;
-    int i = 0;
-    int parent[] = new int[v];
-    for (int j = 0; j < v; j++) {
-      parent[j] = j;
+    public int compareTo(Edge compareEdge) {
+      return this.weight - compareEdge.weight;
     }
-    while (count != v) {
+  };
 
-      Edge currentedge = edges[i++];
-      int v1parent = findparent(currentedge.v1, parent);
-      int v2parent = findparent(currentedge.v2, parent);
-      if (v1parent != v2parent) {
-        // including current edge
-        mst[count] = currentedge;
-        count++;
-        parent[v1parent] = v2parent;
+  // Union
+  class subset {
+    int parent, rank;
+  };
+
+  int vertices, edges;
+  Edge edge[];
+
+  // Graph creation
+  Graph(int v, int e) {
+    vertices = v;
+    edges = e;
+    edge = new Edge[edges];
+    for (int i = 0; i < e; ++i)
+      edge[i] = new Edge();
+  }
+
+  int find(subset subsets[], int i) {
+    if (subsets[i].parent != i)
+      subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+  }
+
+  void Union(subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank)
+      subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+      subsets[yroot].parent = xroot;
+    else {
+      subsets[yroot].parent = xroot;
+      subsets[xroot].rank++;
+    }
+  }
+
+  // Applying Krushkal Algorithm
+  void KruskalAlgo() {
+    Edge result[] = new Edge[vertices];
+    int e = 0;
+    int i = 0;
+    for (i = 0; i < vertices; ++i)
+      result[i] = new Edge();
+
+    // Sorting the edges
+    Arrays.sort(edge);
+    subset subsets[] = new subset[vertices];
+    for (i = 0; i < vertices; ++i)
+      subsets[i] = new subset();
+
+    for (int v = 0; v < vertices; ++v) {
+      subsets[v].parent = v;
+      subsets[v].rank = 0;
+    }
+    i = 0;
+    while (e < vertices - 1) {
+      Edge next_edge = new Edge();
+      next_edge = edge[i++];
+      int x = find(subsets, next_edge.src);
+      int y = find(subsets, next_edge.dest);
+      if (x != y) {
+        result[e++] = next_edge;
+        Union(subsets, x, y);
       }
     }
-    return mst;
-  }
-
-  private static int findparent(int v1, int[] parent) {
-    if (v1 == parent[v1]) {
-      return v1;
-    }
-    return findparent(parent[v1], parent);
+    for (i = 0; i < e; ++i)
+      System.out.println(result[i].src + " - " + result[i].dest + ": " + result[i].weight);
   }
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    // how many vertex
-    int v = sc.nextInt();
-    // how many edges
-    int e = sc.nextInt();
-    Edge edgearray[] = new Edge[e];
-    for (int i = 0; i < e; i++) {
-      int v1 = sc.nextInt();
-      int v2 = sc.nextInt();
-      int weight = sc.nextInt();
-      Edge obj = new Edge(v1, v2, weight);
-      edgearray[i] = obj;
-    }
-    Edge[] mst = kruskal(edgearray, v);
-    for (int i = 0; i < mst.length; i++) {
-      if (mst[i].v1 < mst[i].v2) {
-        System.out.println(mst[i].v1 + " " + mst[i].v2 + "  " + mst[i].weight);
-      } else {
-        System.out.println(mst[i].v2 + " " + mst[i].v1 + "  " + mst[i].weight);
-      }
-    }
-    sc.close();
+    int vertices = 6; // Number of vertices
+    int edges = 8; // Number of edges
+    Graph G = new Graph(vertices, edges);
+
+    G.edge[0].src = 0;
+    G.edge[0].dest = 1;
+    G.edge[0].weight = 4;
+
+    G.edge[1].src = 0;
+    G.edge[1].dest = 2;
+    G.edge[1].weight = 4;
+
+    G.edge[2].src = 1;
+    G.edge[2].dest = 2;
+    G.edge[2].weight = 2;
+
+    G.edge[3].src = 2;
+    G.edge[3].dest = 3;
+    G.edge[3].weight = 3;
+
+    G.edge[4].src = 2;
+    G.edge[4].dest = 5;
+    G.edge[4].weight = 2;
+
+    G.edge[5].src = 2;
+    G.edge[5].dest = 4;
+    G.edge[5].weight = 4;
+
+    G.edge[6].src = 3;
+    G.edge[6].dest = 4;
+    G.edge[6].weight = 3;
+
+    G.edge[7].src = 5;
+    G.edge[7].dest = 4;
+    G.edge[7].weight = 3;
+    G.KruskalAlgo();
   }
 }
